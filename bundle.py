faeb53b30a5bad8a833bb5733b259ca219ae468d
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-09-05 18:39:34 +0300

    Saving and loading bundle data

---
+++ bundle.py
@@ -0,0 +1,48 @@
+from typing import Dict
+import json
+
+
+class Bundle:
+    def __init__(self, name: str, slots_required: int):
+        print(f"Add bundle {name} with {slots_required} slots required")
+        self.name: str = name
+        self.slots_required: int = slots_required
+        self.items: Dict[str, int] = dict()
+
+    def __str__(self) -> str:
+        return f"<Bundle {self.name} - {self.slots_required} slots required - {len(self.items)} items>"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def add_item(self, item: str, quantity: int = 1):
+        print(f"Add {item} ({quantity}) to bundle {self.name}")
+        self.items[item] = quantity
+
+    def to_json_string(self) -> str:
+        return json.dumps(self.__dict__)
+
+    @classmethod
+    def from_json_string(cls, json_string: str) -> 'Bundle':
+        return cls.from_json(json.loads(json_string))
+
+    @classmethod
+    def from_json(cls, json_data: Dict):
+        new_bundle = Bundle(name=json_data['name'], slots_required=json_data['slots_required'])
+
+        for item, quantity in json_data['items'].items():
+            new_bundle.add_item(item=item, quantity=quantity)
+
+        return new_bundle
+
+
+if __name__ == '__main__':
+    test_bundle = Bundle(name="test", slots_required=3)
+    test_bundle.add_item("wood", 99)
+    test_bundle.add_item("mudskipper")
+
+    print(test_bundle.to_json_string())
+    b2 = Bundle.from_json_string(test_bundle.to_json_string())
+    print(b2.to_json_string())
+
+    print(b2)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import Set
 import json
 
+from bundle import Bundle
+
 ITEMS_FILE = "./items.json"
 BUNDLES_FILE = "./bundles.json"
 
@@ -14,10 +16,18 @@ def save_items(items: Set[str]):
         print(f"ERROR SAVING ITEMS: {e}")
 
 
-def save_bundles(bundles: Set[str]):
+def save_bundles(bundles: Set[Bundle]):
+    json_string = "["
+
+    for bundle in bundles:
+        json_string = f"{json_string}\n{bundle.to_json_string()},"
+
+    json_string = json_string[:-1]  # remove last comma
+    json_string = f"{json_string}\n]"
+
     try:
         with open(BUNDLES_FILE, 'w') as f:
-            json.dump(sorted(bundles), f, indent=4)
+            f.write(json_string)
             print(f"Bundles saved")
     except OSError as e:
         print(f"ERROR SAVING ITEMS: {e}")
@@ -25,7 +35,7 @@ def save_bundles(bundles: Set[str]):
 
 def main():
     items: Set[str] = set()
-    bundles: Set[str] = set()
+    bundles: Set[Bundle] = set()
 
     try:
         with open(ITEMS_FILE) as f:
@@ -35,13 +45,18 @@ def main():
 
     try:
         with open(BUNDLES_FILE) as f:
-            bundles = set(json.load(f))
+            loaded_bundles = json.load(f)
+            print(loaded_bundles)
+            for bundle in loaded_bundles:
+                print(bundle)
+                bundles.add(Bundle.from_json(bundle))
     except OSError as e:
         print(e)
 
     while True:
         print(f"Items:")
         print(sorted(items))
+        print(bundles)
 
         print(f"(a)dd items, a(d)d bundle, (q)uit: ")
         user_input = input(": ").lower().strip()
@@ -54,6 +69,30 @@ def main():
                 print(f"Adding item: {item_to_add}")
                 items.add(item_to_add)
                 save_items(items)
+
+        elif user_input == "d":
+            while True:
+                bundle_name = input("Bundle name (q to return): ").lower().strip()
+                if bundle_name == "q":
+                    break
+                slots_required = int(input("Slots required: "))
+
+                new_bundle = Bundle(name=bundle_name, slots_required=slots_required)
+
+                while True:
+                    item_to_add = input("Item to add (q to return): ").lower().strip()
+                    if item_to_add == "q":
+                        break
+                    quantity = input("How many needed? (default 1): ")
+                    try:
+                        quantity = int(quantity)
+                    except ValueError:
+                        quantity = 1
+
+                    new_bundle.add_item(item=item_to_add, quantity=quantity)
+
+                bundles.add(new_bundle)
+                save_bundles(bundles)
 
         elif user_input == "q":
             break
